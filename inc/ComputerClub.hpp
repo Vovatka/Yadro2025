f814613916d871d7e92311aae71bf9af165eeab9
Vovatka <wowatka4enko@yandex.ru>
2025-05-06 23:58:53 +0300

    add all

--- inc/ComputerClub.hpp
+++ inc/ComputerClub.hpp
@@ -11,6 +11,16 @@
 #include <fstream>
 #include <chrono>
 #include <optional>
+#include <map>
+#include <unordered_set>
+#include <queue>
+#include <tuple>
+
+//! Кортеж для хранения номера стола и времени, когда этот стол был занят в последний раз
+using Tuple = std::tuple<int, std::chrono::minutes>; 
+//! Кортеж для хранения имени посетителя, и времени, когда он встал в очередь
+using Waiting = std::tuple<std::string, std::chrono::minutes>;
+
 
 class ComputerClub {
 public:
@@ -31,13 +41,32 @@ public:
     ~ComputerClub();
     /**
     \brief Основной цикл
-    @return - true если цикл пройден полностью, 
+    @return true если цикл пройден полностью, 
     иначе - false
     */
     bool StartLoop();
 private:
     std::string _fileName; //!< имя файла
     std::fstream _data; //!< данные из файла
+    std::chrono::minutes _openTime; //!< Время открытия
+    std::chrono::minutes _closeTime; //!< Время закрытия
+    //! Посетители, пришедшие в клуб
+    std::unordered_set<std::string> _entered_visitors;
+    //! Посетители, ожидающие свободного стола
+    std::queue<Waiting> _waiting_visitors;
+    //! Посетители за столами
+    std::map<std::string, Tuple> _visitors;
+    //! Занятые столы
+    std::map<int, std::string> _tables;
+    std::map<int, Tuple> total; //<! Выручка со столов
+
+    /**
+    \brief Функция для установки времени работы клуба
+    в минутах (сохраняет в поля _openTime и _closeTime)
+    \tparam line - строка с временем открытия и закрытия
+    @return true - если формат времени верный, иначе - false
+    */
+    bool _setTime(std::string& line);
     /**
     \brief Функция для получения кол-ва минут из
     строки в формате "HH:MM"
@@ -45,7 +74,7 @@ private:
     @return nullopt - если есть ошибка в формате, 
     иначе - кол-во минут в chrono::minutes
     */
-    std::optional<std::chrono::minutes> _validateTime(const std::string& str);
+    std::optional<std::chrono::minutes> _getMinutes(const std::string& str);
     /**
     \brief Функция для проверки формата имени клиента
     \tparam str - строка
@@ -56,11 +85,31 @@ private:
     Подразумевается, что кол-во столов > 0
     \tparam str - строка
     */
-    int _validateTabels(const std::string& str);
+    int _validateTables(const std::string& str);
     /**
     \brief Функция для проверки цены за час
     Подразумевается, что цена за час > 0
     \tparam str - строка
     */
     float _validatePrice(const std::string& str);
+    /**
+    \brief Функция для проверки ID клиента
+    \tparam str - строка
+    */
+    int _validateID(const std::string& str);
+    /**
+    \brief Функция для проверки стола, за который 
+    хочет сесть посетитель
+    \tparam str - строка
+    \tparam tables - кол-во столов в клубе
+    */
+    int _validateTable(const std::string& str, int tables);
+    /**
+    \brief Функция для печати ошибки
+    */
+    void printError(const std::string& time, const std::string message) const;
+    /**
+    \brief Функция для перевода chrono::minutes в string
+    */
+    std::string _minutesToString(const std::chrono::minutes& minutes) const;
 };
--- src/ComputerClub.cpp
+++ src/ComputerClub.cpp
@@ -3,6 +3,8 @@
 #include <fstream> 
 #include <sstream>
 #include <regex>
+#include <cmath>
+#include <iomanip>
 
 
 ComputerClub::ComputerClub(const std::string& file): _fileName(file) {
@@ -17,25 +19,24 @@ ComputerClub::ComputerClub(const std::string& file): _fileName(file) {
 bool ComputerClub::StartLoop() {
     std::string line;
 
+
     // узнаем кол-во столов
     std::getline(_data, line);
-    int tabels = _validateTabels(line);
-    if (!tabels) {
+    int tables = _validateTables(line);
+    if (!tables) {
         std::cerr << line << std::endl;
         return false;
+    }
+    for (int i = 1; i <= tables; i++) {
+        total[i] = Tuple(0, *_getMinutes("00:00"));
     }
 
-    // узнаем время
-    std::getline(_data, line);
-    std::istringstream tss{line};
-    std::string t1, t2;
-    tss >> t1; tss >> t2;
-    auto openTime = _validateTime(t1);
-    auto closeTime = _validateTime(t2);
-    if (!openTime || !closeTime) {
+    // узнаем время работы
+    if (!_setTime(line)) {
         std::cerr << line << std::endl;
         return false;
     }
+    
 
     // узнаем цену стола за час
     std::getline(_data, line);
@@ -45,10 +46,153 @@ bool ComputerClub::StartLoop() {
         return false;
     }
 
+    std::string word;
+    int k = 0, id = 0, table = tables;
+    auto prevTime = _getMinutes("00:00");
+    std::vector<std::string> event;
+    std::cout << _minutesToString(_openTime) << std::endl;
+    while (std::getline(_data, line)) {
+        std::istringstream iss{line};
+        while (iss >> word) {
+            event.push_back(word);
+        }
+        auto time = _getMinutes(event[0]);
+        id = _validateID(event[1]);
+        std::string client = event[2];
+
+        // проверяем формат данных о клиенте
+        if (id == 2) {
+            table = _validateTable(event[3], tables);
+        }
+
+        if (!(time && id && table && _isValidName(client) && event.size() <= 4 && prevTime < *time)) {
+            std::cerr << line << std::endl;
+            return false;
+        }
+
+        std::cout << line << std::endl;
+
+//         ID 1. Клиент пришел
+// Формат: <время> 1 <имя клиента>
+// Если клиент уже в компьютерном клубе, генерируется ошибка "YouShallNotPass"
+// Если клиент пришел в нерабочие часы, тогда "NotOpenYet"
+
+        if (id == 1) {
+            if (_openTime > time.value()) {
+                printError(event[2], "NotOpenYet");
+            }
+            else if (_entered_visitors.contains(client)) {
+                printError(event[2], "YouShallNotPass"); 
+            }
+            else {
+                _entered_visitors.insert(client);
+            }
+        }
+
+        else if (id == 2) {
+            if (_tables.contains(table)) {
+                printError(event[2], "PlaceIsBusy");
+            }
+            else if (!_entered_visitors.contains(client)) {
+                printError(event[2], "ClientUnknown");
+            }
+            else {
+                _tables[table] = client;
+                _visitors[client] =  Tuple{table, *time};
+                std::cout << event[2] << " " << 12 << " " << client << " " << table << std::endl;
+            }
+        }
+
+
+//         ID 3. Клиент ожидает
+// Формат: <время> 3 <имя клиента>
+// Если в клубе есть свободные столы, то генерируется ошибка "ICanWaitNoLonger!".
+// Если в очереди ожидания клиентов больше, чем общее число столов, то клиент уходит и генерируется событие ID 11.
+
+        else if (id  == 3) {
+            if (_tables.size() < tables) {
+                printError(event[2], "ICanWaitNoLonger!");
+            }
+            else if (_waiting_visitors.size() > tables) {
+                std::cout << client[2] << " " << 11 <<  " " << client << std::endl;
+                _entered_visitors.erase(client);
+            }
+            else {
+                _waiting_visitors.push(Waiting{client, *time});
+            }
+        }
+
+// ID 4. Клиент ушел
+// Формат: <время> 4 <имя клиента>
+// Если клиент не находится в компьютерном клубе, генерируется ошибка "ClientUnknown".
+// Когда клиент уходит, стол, за которым он сидел освобождается и его занимает первый клиент из очереди ожидания (ID 12).
+
+        else {
+            if (!_entered_visitors.contains(client)) {
+                printError(event[2], "ClientUnknown");
+            }
+            else {
+                auto [place1, t] = _visitors[client];
+                auto [all_price, all_time] = total[place1];
+                all_time += t;
+                double hours = std::chrono::duration<double, std::ratio<3600>>(t).count();
+                all_price += static_cast<int>(std::ceil(hours)) * price;
+                total[place1] = Tuple(all_price, all_time);
+
+                _visitors.erase(client);
+                auto [wclient, mins] = _waiting_visitors.front();
+                _waiting_visitors.pop();
+                std::cout << event[2] << " " << 11 << " " << client << std::endl;
+                _visitors[wclient] = Tuple(place1, mins);
+                _tables[place1] = wclient;
+                std::cout << event[2] << " " << 12 << " " << wclient << " " << place1 << std::endl;
+            }
+        }
+
+        event.clear();
+        prevTime = *time;
+    }
+
+    std::cout << _minutesToString(_closeTime) << std::endl;
+    for(auto&& [client, tuple]: _visitors) {
+        auto [place1, t] = tuple;
+        auto [all_price, all_time] = total[place1];
+        all_time += t;
+        int total_minutes = t.count();
+        all_price += static_cast<int>(std::ceil(total_minutes / 60.0));
+        std::cout << _minutesToString(t) << " " << 11 << " " << client << std::endl;
+        total[place1] = Tuple(all_price, all_time);
+    }
+
+    while (!_waiting_visitors.empty()) {
+        auto [client, time] = _waiting_visitors.front();
+        std::cout << _minutesToString(time) << " " << 11 << " " << client << std::endl;
+        _waiting_visitors.pop();
+    }
+
+    // for (auto&& [key, value]: total) {
+    //     auto [all_price, all_time] = value;
+    //     std::cout << key << all_price << _minutesToString(all_time);
+    // }
     return true;
 }
 
-std::optional<std::chrono::minutes> ComputerClub::_validateTime(const std::string& str) {
+bool ComputerClub::_setTime(std::string& line) {
+    std::getline(_data, line);
+    std::istringstream tss{line};
+    std::string t1, t2;
+    tss >> t1; tss >> t2;
+    auto openTime = _getMinutes(t1);
+    auto closeTime = _getMinutes(t2);
+    if (openTime && closeTime) {
+        _openTime = *openTime;
+        _closeTime = *closeTime;
+        return true;
+    };
+    return false;
+}
+
+std::optional<std::chrono::minutes> ComputerClub::_getMinutes(const std::string& str) {
     std::regex time_r(R"(^\d{2}:\d{2}$)");
     if ( !std::regex_match(str, time_r) ) {
         return std::nullopt;
@@ -68,13 +212,13 @@ bool ComputerClub::_isValidName(const std::string& str) {
 }
 
 
-int ComputerClub::_validateTabels(const std::string& str) {
+int ComputerClub::_validateTables(const std::string& str) {
     try {
-        int tabels = std::stoi(str);
-        if (tabels <= 0) {
+        int tables = std::stoi(str);
+        if (tables <= 0) {
             throw std::invalid_argument("");
         }
-        return tabels;
+        return tables;
     }
     catch (const std::invalid_argument& e) {
         return 0;
@@ -101,6 +245,47 @@ float ComputerClub::_validatePrice(const std::string& str) {
     }
 }
 
+int ComputerClub::_validateID(const std::string& str) {
+    try {
+        int id = std::stoi(str);
+        if (id <= 0 || id > 4) {
+            throw std::invalid_argument("");
+        }
+        return id;
+    }
+    catch (const std::invalid_argument& e) {
+        return 0;
+    }
+    catch (const std::out_of_range& e) {
+        return 0;
+    }
+}
+
+int ComputerClub::_validateTable(const std::string& str, int tables) { 
+    int t = _validateTables(str);
+    return t && t <= tables ? t : 0;
+}
+
+void ComputerClub::printError(const std::string& time, const std::string message) const {
+    std::cout << time << " " << 13 << " " << message << std::endl;
+}
+
+std::string ComputerClub::_minutesToString(const std::chrono::minutes& minutes) const {
+    int total_minutes = minutes.count();
+    
+    int hours = total_minutes / 60;
+    int mins = total_minutes % 60;
+
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << hours
+        << ":"
+        << std::setw(2) << std::setfill('0') << mins;
+
+    return oss.str();
+}
+
 ComputerClub::~ComputerClub() {
     _data.close();
 }
+
+
